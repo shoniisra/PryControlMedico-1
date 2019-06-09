22a3620c7bb6db84384cff35b54dce7500b2f1e9
Shoni Isra <villajohnny@gmail.com>
2019-06-09 13:53:05 -0500

    Actualizacion controladores

--- src/main/java/com/medico/app/web/controllers/PacienteController.java
+++ src/main/java/com/medico/app/web/controllers/PacienteController.java
@@ -6,10 +6,13 @@ import com.medico.app.web.models.services.IPacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -25,14 +28,23 @@ public class PacienteController {
     @GetMapping(value="/create" )
     public String create(Model model){
         Paciente paciente=new Paciente();
+        model.addAttribute("title","Registro de nuevo paciente");
         model.addAttribute("paciente",paciente);
         return "paciente/form";
     }
 
     @PostMapping(value="/save" )
-    public String save(Paciente paciente,Model model){
+    public String save(Paciente paciente, BindingResult result,
+                       Model model,
+                       RedirectAttributes message, SessionStatus session){
         try{
+            if(result.hasErrors()) {
+                return "paciente/form";
+            }
+            String msg = paciente.getIdpersona() == null ? paciente.getNombre() + " ha sido agregado." : paciente.getNombre() + " ha sido actualizado.";
             service.save(paciente);
+            session.setComplete();
+            message.addFlashAttribute("success", msg);
         }catch (Exception ex){
             model.addAttribute("error: ",ex.toString());
         }
@@ -44,6 +56,7 @@ public class PacienteController {
                            Model model){
         Paciente paciente=service.findById(id);
         model.addAttribute("paciente",paciente);
+        model.addAttribute("title","Actualización de paciente: " + paciente.getNombre());
         return "paciente/card";
     }
 
@@ -55,11 +68,12 @@ public class PacienteController {
         return "paciente/form";
     }
 
-    @GetMapping(value="/delete" )
+    @GetMapping(value="/delete/{id}" )
     public String delete(@PathVariable(value = "id") Integer id,
-                         Model model){
+                         Model model, RedirectAttributes message){
         try{
             service.delete(id);
+            message.addFlashAttribute("success", "Paciente eliminado correctamente");
         }catch (Exception ex){
             model.addAttribute("error: ",ex.toString());
         }
@@ -70,6 +84,7 @@ public class PacienteController {
     public String list(Model model){
         List<Paciente> pacientes=service.findAll();
         model.addAttribute("lista",pacientes);
+        model.addAttribute("title","Listado de pacientes");
         return "paciente/list";
     }
 }

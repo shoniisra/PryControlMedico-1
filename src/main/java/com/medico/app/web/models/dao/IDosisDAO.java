e7796d95bcf3628da2dd0e59db30f637ff9300aa
Anthony <huasipango@gmail.com>
2019-07-06 18:21:14 -0500

    Tarea aceptada. Tres estados: pendiente, rechazado y suministrado.

--- src/main/java/com/medico/app/web/models/dao/IDosisDAO.java
+++ src/main/java/com/medico/app/web/models/dao/IDosisDAO.java
@@ -8,6 +8,6 @@ import com.medico.app.web.models.entities.Dosis;
 import java.util.List;
 
 public interface IDosisDAO extends CrudRepository<Dosis, Integer> {
-    @Query("SELECT D FROM Dosis D WHERE D.detalleReceta.iddetalleReceta = ?1 AND D.estado <> 1")
+    @Query("SELECT D FROM Dosis D WHERE D.detalleReceta.iddetalleReceta = ?1 AND D.estado = 0")
     public List<Dosis> findNotTakenPills(Integer idDetalleReceta);
 }
